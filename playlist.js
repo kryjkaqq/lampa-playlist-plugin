848d77fd3c62b9413cd0d86a082ebe158d9429e0
Ignatiev Artem <ignatiev.ak@edu.spbstu.ru>
2026-07-21 18:42:54 +0300

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -1,20 +1,36 @@
 (function () {
     'use strict';
 
-    function plugin() {
-        // Уведомление при запуске
+    function showVisualBanner(text) {
+        try {
+            // Создаём яркую плашку поверх всего интерфейса Lampa
+            var div = document.createElement('div');
+            div.style.cssText = 'position:fixed;top:20px;left:50%;transform:translateX(-50%);background:#ff0055;color:#fff;padding:15px 25px;font-size:18px;z-index:999999;border-radius:8px;box-shadow:0 0 15px rgba(0,0,0,0.8);font-weight:bold;font-family:sans-serif;';
+            div.innerText = text;
+            document.body.appendChild(div);
+
+            setTimeout(function() {
+                if (div && div.parentNode) div.parentNode.removeChild(div);
+            }, 5000);
+        } catch(e) {}
+    }
+
+    function startPlugin() {
+        // 1. Визуальный баннер в DOM
+        showVisualBanner('M3U TorrServer: Плагин загружен!');
+
+        // 2. Стандартное уведомление Lampa
         if (typeof Lampa !== 'undefined' && Lampa.Noty) {
-            Lampa.Noty.show('M3U Плагин загружен!');
+            Lampa.Noty.show('M3U TorrServer: Активен');
         }
 
-        // Перехватываем воспроизведение
+        // 3. Перехват плеера
         if (typeof Lampa !== 'undefined' && Lampa.Player) {
             var originalPlay = Lampa.Player.play;
 
             Lampa.Player.play = function (item) {
                 try {
                     if (item && item.url && (item.url.indexOf('link=') !== -1 || item.url.indexOf('hash=') !== -1)) {
-                        // Разбираем URL без использования new URL (для старых движков)
                         var match = item.url.match(/(https?:\/\/[^\/]+)/);
                         var hashMatch = item.url.match(/(?:link|hash)=([a-fA-F0-9]+)/);
 
@@ -22,16 +38,16 @@
                             var host = match[1];
                             var hash = hashMatch[1];
                             
-                            // Заменяем ссылку на M3U плейлист TorrServer
+                            // Формируем M3U плейлист
                             item.url = host + '/playlist.m3u?hash=' + hash;
 
                             if (Lampa.Noty) {
-                                Lampa.Noty.show('Отправлен M3U плейлист');
+                                Lampa.Noty.show('M3U плейлист отправлен в плеер');
                             }
                         }
                     }
                 } catch (e) {
-                    console.error('Plugin Error:', e);
+                    console.error('Playlist Patch Error:', e);
                 }
 
                 return originalPlay.call(this, item);
@@ -39,14 +55,17 @@
         }
     }
 
-    // Безопасная инициализация
-    if (window.appready) {
-        plugin();
+    // Инициализация при любом состоянии готовности
+    if (window.appready || (typeof Lampa !== 'undefined' && Lampa.Player)) {
+        startPlugin();
     } else {
         if (typeof Lampa !== 'undefined' && Lampa.Listener) {
             Lampa.Listener.follow('app', function (e) {
-                if (e.type === 'ready') plugin();
+                if (e.type === 'ready') startPlugin();
             });
+        } else {
+            // Если Listener недоступен, пробуем запустить через 2 секунды
+            setTimeout(startPlugin, 2000);
         }
     }
 })();
