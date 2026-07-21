e72a36fe3f84068cea54f788fa6c13d67c9b0e8a
Ignatiev Artem <ignatiev.ak@edu.spbstu.ru>
2026-07-21 18:35:38 +0300

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -1,50 +1,22 @@
 (function () {
     'use strict';
 
-    function init() {
-        if (typeof Lampa === 'undefined' || !Lampa.Player) return;
-
-        // Запоминаем оригинальную функцию запуска видео в Lampa
-        const originalPlay = Lampa.Player.play;
-
-        Lampa.Player.play = function (item) {
-            try {
-                const data = Lampa.Player.playdata();
-
-                if (data && Array.isArray(data.playlist) && data.playlist.length > 1) {
-                    // Проверяем, идет ли проигрывание через TorrServer
-                    if (item && item.url && item.url.includes('link=')) {
-                        const urlObj = new URL(item.url);
-                        const hash = urlObj.searchParams.get('link') || urlObj.searchParams.get('hash');
-
-                        if (hash) {
-                            // Формируем прямую M3U ссылку TorrServer
-                            const m3uUrl = `${urlObj.origin}/playlist.m3u?hash=${hash}`;
-
-                            // Подменяем URL текущего элемента на весь плейлист
-                            item.url = m3uUrl;
-
-                            if (Lampa.Noty) {
-                                Lampa.Noty.show('Подменен плейлист TorrServer (M3U)');
-                            }
-                        }
-                    }
-                }
-            } catch (e) {
-                console.error('Ошибка в плагине плейлиста:', e);
-            }
-
-            // Вызываем родной метод Lampa
-            return originalPlay.call(this, item);
-        };
+    // Выводим всплывающее окно прямо в интерфейсе Lampa при старте
+    function test() {
+        if (typeof Lampa !== 'undefined' && Lampa.Noty) {
+            Lampa.Noty.show('ТЕСТ: Плагин успешно загружен!');
+        } else {
+            alert('ТЕСТ: Плагин загружен!');
+        }
     }
 
-    // Запускаем после полной загрузки Lampa
     if (window.appready) {
-        init();
+        test();
     } else {
-        Lampa.Listener.follow('app', function (e) {
-            if (e.type === 'ready') init();
-        });
+        if (typeof Lampa !== 'undefined' && Lampa.Listener) {
+            Lampa.Listener.follow('app', function (e) {
+                if (e.type === 'ready') test();
+            });
+        }
     }
 })();
