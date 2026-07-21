247e5c90784fdb4291211fb49cd60021aa3b80d7
Ignatiev Artem <ignatiev.ak@edu.spbstu.ru>
2026-07-21 19:34:15 +0300

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -3,7 +3,7 @@
 
     function startPlugin() {
         if (typeof Lampa !== 'undefined' && Lampa.Noty) {
-            Lampa.Noty.show('M3U TorrServer: Активен');
+            Lampa.Noty.show('M3U TorrServer (API): Активен');
         }
 
         if (typeof Lampa !== 'undefined' && Lampa.Player) {
@@ -21,29 +21,30 @@
                             var hash = hashMatch[1];
                             var currentFileIndex = indexMatch ? parseInt(indexMatch[1], 10) : 0;
 
-                            // 1. Полностью сбрасываем историю просмотров (View States) для этого торрента
+                            // 1. Сбрасываем старую историю просмотров через чистый API
                             try {
                                 var resetXhr = new XMLHttpRequest();
                                 resetXhr.open('POST', host + '/viewed', false);
                                 resetXhr.setRequestHeader('Content-Type', 'application/json');
                                 resetXhr.send(JSON.stringify({ action: 'rem', hash: hash }));
-                            } catch (e) {
-                                // Игнорируем ошибки сети
-                            }
+                            } catch (e) {}
 
-                            // 2. Если выбрана серия > 1 (индекс > 0), отмечаем предыдущую, 
-                            // чтобы fromlast отдал плейлист НАЧИНАЯ с выбранной серии.
+                            // 2. Если серия > 1, метку "просмотрено" ставим через API без загрузки файла (без &play и без stream)
                             if (currentFileIndex > 0) {
                                 var targetIndex = currentFileIndex - 1;
-                                var pingUrl = host + '/stream/file.mkv?link=' + hash + '&index=' + targetIndex;
                                 try {
-                                    var pingXhr = new XMLHttpRequest();
-                                    pingXhr.open('GET', pingUrl, false);
-                                    pingXhr.send();
+                                    var setXhr = new XMLHttpRequest();
+                                    setXhr.open('POST', host + '/viewed', false);
+                                    setXhr.setRequestHeader('Content-Type', 'application/json');
+                                    setXhr.send(JSON.stringify({
+                                        action: 'set',
+                                        hash: hash,
+                                        file_index: targetIndex
+                                    }));
                                 } catch (e) {}
                             }
 
-                            // 3. Отдаем чистый URL плейлиста
+                            // 3. Передаем чистую ссылку на плейлист БЕЗ флага &play на конце!
                             item.url = host + '/stream/playlist.m3u?link=' + hash + '&m3u&fromlast';
 
                             if (Lampa.Noty) {
