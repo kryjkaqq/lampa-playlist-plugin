a41b0e096b179d314898b2bbcbde325b522a614a
Ignatiev Artem <ignatiev.ak@edu.spbstu.ru>
2026-07-21 18:56:46 +0300

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -14,17 +14,27 @@
                     if (item && item.url && (item.url.indexOf('link=') !== -1 || item.url.indexOf('hash=') !== -1)) {
                         var hostMatch = item.url.match(/(https?:\/\/[^\/]+)/);
                         var hashMatch = item.url.match(/(?:link|hash)=([a-fA-F0-9]+)/);
+                        // Извлекаем id/index выбранного файла (серии)
+                        var indexMatch = item.url.match(/(?:index|id|file)=([0-9]+)/);
 
                         if (hostMatch && hashMatch) {
                             var host = hostMatch[1];
                             var hash = hashMatch[1];
                             var title = item.title ? encodeURIComponent(item.title) : 'playlist';
+                            var fileIndex = indexMatch ? indexMatch[1] : null;
 
-                            // Исправленный адрес эндпоинта TorrServer MatriX
-                            item.url = host + '/stream/' + title + '.m3u?link=' + hash + '&m3u';
+                            // Формируем ссылку на плейлист
+                            var playlistUrl = host + '/stream/' + title + '.m3u?link=' + hash + '&m3u';
+
+                            // Если передана конкретная серия — добавляем указатель на неё
+                            if (fileIndex !== null) {
+                                playlistUrl += '&index=' + fileIndex + '&play=' + fileIndex;
+                            }
+
+                            item.url = playlistUrl;
 
                             if (Lampa.Noty) {
-                                Lampa.Noty.show('Формируем M3U плейлист...');
+                                Lampa.Noty.show('M3U: Запуск с выбранной серии');
                             }
                         }
                     }
