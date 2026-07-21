01cb69a517e8fdf9a2c0ac8f761261a7d9fa2bb9
Ignatiev Artem <ignatiev.ak@edu.spbstu.ru>
2026-07-21 18:46:18 +0300

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -2,18 +2,15 @@
     'use strict';
 
     function startPlugin() {
-        // Уведомление о готовности плагина
         if (typeof Lampa !== 'undefined' && Lampa.Noty) {
-            Lampa.Noty.show('M3U TorrServer: Готов');
+            Lampa.Noty.show('M3U TorrServer: Активен');
         }
 
-        // Перехватываем запуск плеера
         if (typeof Lampa !== 'undefined' && Lampa.Player) {
             var originalPlay = Lampa.Player.play;
 
             Lampa.Player.play = function (item) {
                 try {
-                    // Проверяем, что ссылка идет от TorrServer
                     if (item && item.url && (item.url.indexOf('link=') !== -1 || item.url.indexOf('hash=') !== -1)) {
                         var match = item.url.match(/(https?:\/\/[^\/]+)/);
                         var hashMatch = item.url.match(/(?:link|hash)=([a-fA-F0-9]+)/);
@@ -22,11 +19,12 @@
                             var host = match[1];
                             var hash = hashMatch[1];
                             
-                            // Заменяем одиночную ссылку на полный M3U плейлист
-                            item.url = host + '/playlist.m3u?hash=' + hash;
+                            // Формируем M3U ссылкой со всеми параметрами (link + hash + title)
+                            var title = item.title ? encodeURIComponent(item.title) : 'playlist';
+                            item.url = host + '/playlist.m3u?link=' + hash + '&hash=' + hash + '&title=' + title;
 
                             if (Lampa.Noty) {
-                                Lampa.Noty.show('Передан M3U плейлист серий!');
+                                Lampa.Noty.show('M3U плейлист отправлен');
                             }
                         }
                     }
@@ -34,13 +32,11 @@
                     console.error('Playlist Patch Error:', e);
                 }
 
-                // Передаём обновленный объект дальше в плеер
                 return originalPlay.call(this, item);
             };
         }
     }
 
-    // Запуск
     if (window.appready || (typeof Lampa !== 'undefined' && Lampa.Player)) {
         startPlugin();
     } else {
