375d0b8319c1522f583288d18b32f18533f491ff
Ignatiev Artem <ignatiev.ak@edu.spbstu.ru>
2026-07-21 19:17:01 +0300

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -19,20 +19,42 @@
                         if (hostMatch && hashMatch) {
                             var host = hostMatch[1];
                             var hash = hashMatch[1];
-                            
-                            // Забираем индекс, передаваемый Лампой
-                            var rawIndex = indexMatch ? parseInt(indexMatch[1], 10) : 0;
-                            
-                            var title = item.title ? encodeURIComponent(item.title) : 'playlist';
+                            var startIndex = indexMatch ? parseInt(indexMatch[1], 10) : 0;
 
-                            // Формируем чистый HTTP URL плейлиста без data-uri
-                            // Параметр from указывает TorrServer, с какого именно файла отдать плейлист
-                            var playlistUrl = host + '/stream/' + title + '.m3u?link=' + hash + '&from=' + rawIndex + '&m3u';
+                            // 1. Запрашиваем информацию о файлах торрента напрямую из API TorrServer
+                            var xhr = new XMLHttpRequest();
+                            xhr.open('POST', host + '/torrents', false); // Синхронный запрос к API
+                            xhr.setRequestHeader('Content-Type', 'application/json');
+                            xhr.send(JSON.stringify({ action: 'get', hash: hash }));
 
-                            item.url = playlistUrl;
+                            if (xhr.status === 200 && xhr.responseText) {
+                                var response = JSON.parse(xhr.responseText);
+                                var files = (response.file_stats || response.files || []);
 
-                            if (Lampa.Noty) {
-                                Lampa.Noty.show('Плейлист с выбранной серии');
+                                if (files.length > 0) {
+                                    var m3uLines = ['#EXTM3U'];
+
+                                    // 2. Формируем список ссылок строго начиная с выбранного startIndex (серии)
+                                    for (var i = startIndex; i < files.length; i++) {
+                                        var file = files[i];
+                                        var fileId = file.id !== undefined ? file.id : i;
+                                        var fileName = file.path ? file.path.split('/').pop() : ('Episode ' + (fileId + 1) + '.mkv');
+                                        
+                                        // Формируем прямую ссылку на каждый файл, как вы предложили
+                                        var fileUrl = host + '/stream/' + encodeURIComponent(fileName) + '?link=' + hash + '&index=' + fileId + '&play';
+
+                                        m3uLines.push('#EXTINF:-1,' + fileName);
+                                        m3uLines.push(fileUrl);
+                                    }
+
+                                    // 3. Преобразуем собранный плейлист в Data-URL без обрезаний
+                                    var m3uContent = m3uLines.join('\n');
+                                    item.url = 'data:audio/mpegurl;charset=utf-8,' + encodeURIComponent(m3uContent);
+
+                                    if (Lampa.Noty) {
+                                        Lampa.Noty.show('Сформирован плейлист: с серии №' + (startIndex + 1));
+                                    }
+                                }
                             }
                         }
                     }
